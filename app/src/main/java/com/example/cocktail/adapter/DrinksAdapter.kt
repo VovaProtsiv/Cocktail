e63f065ea23a43334cb34772dc14cfadeb2cf4a5
VovaProtsiv <vovapro111@gmail.com>
2020-05-01 22:19:35 +0300

    add adapter

---
+++ app/src/main/java/com/example/cocktail/adapter/DrinksAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.cocktail.adapter
+
+import Drink
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cocktail.R
+
+class DrinksAdapter(internal val context: Context, internal var drinkList: List<Drink>) :
+    RecyclerView.Adapter<DrinksViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int):DrinksViewHolder {
+        val itemView = LayoutInflater.from(p0.context)
+            .inflate(R.layout.drinks_layout, p0, false)
+        return DrinksViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return drinkList.size
+    }
+
+    override fun onBindViewHolder(holder: DrinksViewHolder, position: Int) {
+        holder.strDrink.text = drinkList[position].strDrink
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cocktail/adapter/DrinksViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.cocktail.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.drinks_layout.view.*
+
+class DrinksViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+val strDrink = itemView.txt_strDrink
+
+}
