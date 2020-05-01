64e162fc8c79ef876b6e2ce4c2c1221e34a35187
VovaProtsiv <vovapro111@gmail.com>
2020-05-01 17:48:14 +0300

    add SearchScreen

--- app/src/main/java/com/example/cocktail/MainActivity.kt
+++ app/src/main/java/com/example/cocktail/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cocktail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun changeActivity(view: View){
+        val intent = Intent(this, SearchScreen::class.java)
+        startActivity(intent)
     }
 }
