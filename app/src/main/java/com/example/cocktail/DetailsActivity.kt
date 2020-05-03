765ab6e956760d1491d89ea8280fa91388d44e08
VovaProtsiv <vovapro111@gmail.com>
2020-05-03 23:45:27 +0300

    added check for existing drink in db

--- app/src/main/java/com/example/cocktail/DetailsActivity.kt
+++ app/src/main/java/com/example/cocktail/DetailsActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.executor.GlideExecutor.UncaughtThrowableStrategy.LOG
 import com.example.cocktail.adapter.IngredientsRecyclerViewAdapter
 import com.example.cocktail.adapter.MeasureRecyclerViewAdapter
 import com.example.cocktail.dao.DbHelper
@@ -63,24 +64,26 @@ class DetailsActivity() : AppCompatActivity() {
         setContentView(R.layout.activity_details)
         val bundle = intent.extras
 
-       val sharedBookingObject = bundle!!.getParcelable<Drink>("listDrinks")
+        val sharedBookingObject = bundle!!.getParcelable<Drink>("listDrinks")
 
         Glide
             .with(this)
             .load(sharedBookingObject?.strDrinkThumb)
             .into(findViewById<ImageView>(R.id.drinkImgDetail))
 
-        findViewById<TextView>(R.id.nameDetails).apply { text= sharedBookingObject?.strDrink}
-        findViewById<TextView>(R.id.alcoholicDetails).apply { text= sharedBookingObject?.strAlcoholic}
-        findViewById<TextView>(R.id.glassDetails).apply { text= sharedBookingObject?.strGlass}
+        findViewById<TextView>(R.id.nameDetails).apply { text = sharedBookingObject?.strDrink }
+        findViewById<TextView>(R.id.alcoholicDetails).apply {
+            text = sharedBookingObject?.strAlcoholic
+        }
+        findViewById<TextView>(R.id.glassDetails).apply { text = sharedBookingObject?.strGlass }
 
 
 
         recycler_ingredients.setHasFixedSize(true)
-        recycler_ingredients.layoutManager =LinearLayoutManager(this)
+        recycler_ingredients.layoutManager = LinearLayoutManager(this)
 
         recycler_measure.setHasFixedSize(true)
-        recycler_measure.layoutManager =LinearLayoutManager(this)
+        recycler_measure.layoutManager = LinearLayoutManager(this)
 
         val listIngredient = ArrayList<String>()
 
@@ -112,76 +115,94 @@ class DetailsActivity() : AppCompatActivity() {
         sharedBookingObject?.strMeasure7?.let { listMeasure.add(it) }
         sharedBookingObject?.strMeasure8?.let { listMeasure.add(it) }
         sharedBookingObject?.strMeasure9?.let { listMeasure.add(it) }
-        sharedBookingObject?.strMeasure10?.let {listMeasure.add(it) }
-        sharedBookingObject?.strMeasure11?.let {listMeasure.add(it) }
-        sharedBookingObject?.strMeasure12?.let {listMeasure.add(it) }
-        sharedBookingObject?.strMeasure13?.let {listMeasure.add(it) }
-        sharedBookingObject?.strMeasure14?.let {listMeasure.add(it) }
-        sharedBookingObject?.strMeasure15?.let {listMeasure.add(it) }
+        sharedBookingObject?.strMeasure10?.let { listMeasure.add(it) }
+        sharedBookingObject?.strMeasure11?.let { listMeasure.add(it) }
+        sharedBookingObject?.strMeasure12?.let { listMeasure.add(it) }
+        sharedBookingObject?.strMeasure13?.let { listMeasure.add(it) }
+        sharedBookingObject?.strMeasure14?.let { listMeasure.add(it) }
+        sharedBookingObject?.strMeasure15?.let { listMeasure.add(it) }
 
         displayMeasure(listMeasure)
         displayIngredient(listIngredient)
 
-        findViewById<TextView>(R.id.instructions).apply { text= sharedBookingObject?.strInstructions}
-        dbHelper = DbHelper(this,DATABASE_NAME, DATABASE_VERSION)
-
-     val writableDatabase = dbHelper.writableDatabase
-     //writableDatabase.execSQL("drop table if exists $TABLE_DRINKS")
-
+        findViewById<TextView>(R.id.instructions).apply {
+            text = sharedBookingObject?.strInstructions
+        }
+        dbHelper = DbHelper(this, DATABASE_NAME, DATABASE_VERSION)
 
 
-        val contentValues = ContentValues()
-        contentValues.put(KEY_ID,sharedBookingObject?.idDrink )
-        contentValues.put(KEY_NAME,sharedBookingObject?.strDrink )
-        contentValues.put(KEY_IMAGE,sharedBookingObject?.strDrinkThumb )
-        contentValues.put(KEY_ALCOHOLIC,sharedBookingObject?.strAlcoholic )
-        contentValues.put(KEY_GLASS,sharedBookingObject?.strGlass )
-        contentValues.put(KEY_INSTRUCTIONS,sharedBookingObject?.strInstructions )
-        contentValues.put(KEY_INGREDIENT1,sharedBookingObject?.strIngredient1 )
-        contentValues.put(KEY_INGREDIENT2,sharedBookingObject?.strIngredient2 )
-        contentValues.put(KEY_INGREDIENT3,sharedBookingObject?.strIngredient3 )
-        contentValues.put(KEY_INGREDIENT4,sharedBookingObject?.strIngredient4 )
-        contentValues.put(KEY_INGREDIENT5,sharedBookingObject?.strIngredient5 )
-        contentValues.put(KEY_INGREDIENT6,sharedBookingObject?.strIngredient6 )
-        contentValues.put(KEY_INGREDIENT7,sharedBookingObject?.strIngredient7 )
-        contentValues.put(KEY_INGREDIENT8,sharedBookingObject?.strIngredient8 )
-        contentValues.put(KEY_INGREDIENT9,sharedBookingObject?.strIngredient9 )
-        contentValues.put(KEY_INGREDIENT10,sharedBookingObject?.strIngredient10 )
-        contentValues.put(KEY_INGREDIENT11,sharedBookingObject?.strIngredient11 )
-        contentValues.put(KEY_INGREDIENT12,sharedBookingObject?.strIngredient12 )
-        contentValues.put(KEY_INGREDIENT13,sharedBookingObject?.strIngredient13 )
-        contentValues.put(KEY_INGREDIENT14,sharedBookingObject?.strIngredient14 )
-        contentValues.put(KEY_INGREDIENT15,sharedBookingObject?.strIngredient15 )
-        contentValues.put(KEY_MEASURE1,sharedBookingObject?.strMeasure1 )
-        contentValues.put(KEY_MEASURE2,sharedBookingObject?.strMeasure2 )
-        contentValues.put(KEY_MEASURE3,sharedBookingObject?.strMeasure3 )
-        contentValues.put(KEY_MEASURE4,sharedBookingObject?.strMeasure4 )
-        contentValues.put(KEY_MEASURE5,sharedBookingObject?.strMeasure5 )
-        contentValues.put(KEY_MEASURE6,sharedBookingObject?.strMeasure6 )
-        contentValues.put(KEY_MEASURE7,sharedBookingObject?.strMeasure7 )
-        contentValues.put(KEY_MEASURE8,sharedBookingObject?.strMeasure8 )
-        contentValues.put(KEY_MEASURE9,sharedBookingObject?.strMeasure9 )
-        contentValues.put(KEY_MEASURE10,sharedBookingObject?.strMeasure10 )
-        contentValues.put(KEY_MEASURE11,sharedBookingObject?.strMeasure11 )
-        contentValues.put(KEY_MEASURE12,sharedBookingObject?.strMeasure12 )
-        contentValues.put(KEY_MEASURE13,sharedBookingObject?.strMeasure13 )
-        contentValues.put(KEY_MEASURE14,sharedBookingObject?.strMeasure14 )
-        contentValues.put(KEY_MEASURE15,sharedBookingObject?.strMeasure15 )
+        val writableDatabase = dbHelper.writableDatabase
+        //writableDatabase.execSQL("drop table if exists $TABLE_DRINKS")
+        val rawQuery = writableDatabase.rawQuery(
+            "SELECT * FROM $TABLE_DRINKS WHERE $KEY_ID = '" + sharedBookingObject?.idDrink.toString() + "'",
+            null
+        )
+        val id = sharedBookingObject?.idDrink.toString()
+        var idFromDb: String? = null
+        Log.d("-id ", sharedBookingObject?.idDrink.toString())
+        val columnIndexID = rawQuery.getColumnIndex(DbHelper.KEY_ID)
+        if (rawQuery.moveToFirst()) {
+            idFromDb = rawQuery.getString(columnIndexID)
+        }
 
 
-         writableDatabase.insert(TABLE_DRINKS, null, contentValues)
+        rawQuery.close()
+
+        if (!id.equals(idFromDb)) {
+            val contentValues = ContentValues()
+            contentValues.put(KEY_ID, sharedBookingObject?.idDrink)
+            contentValues.put(KEY_NAME, sharedBookingObject?.strDrink)
+            contentValues.put(KEY_IMAGE, sharedBookingObject?.strDrinkThumb)
+            contentValues.put(KEY_ALCOHOLIC, sharedBookingObject?.strAlcoholic)
+            contentValues.put(KEY_GLASS, sharedBookingObject?.strGlass)
+            contentValues.put(KEY_INSTRUCTIONS, sharedBookingObject?.strInstructions)
+            contentValues.put(KEY_INGREDIENT1, sharedBookingObject?.strIngredient1)
+            contentValues.put(KEY_INGREDIENT2, sharedBookingObject?.strIngredient2)
+            contentValues.put(KEY_INGREDIENT3, sharedBookingObject?.strIngredient3)
+            contentValues.put(KEY_INGREDIENT4, sharedBookingObject?.strIngredient4)
+            contentValues.put(KEY_INGREDIENT5, sharedBookingObject?.strIngredient5)
+            contentValues.put(KEY_INGREDIENT6, sharedBookingObject?.strIngredient6)
+            contentValues.put(KEY_INGREDIENT7, sharedBookingObject?.strIngredient7)
+            contentValues.put(KEY_INGREDIENT8, sharedBookingObject?.strIngredient8)
+            contentValues.put(KEY_INGREDIENT9, sharedBookingObject?.strIngredient9)
+            contentValues.put(KEY_INGREDIENT10, sharedBookingObject?.strIngredient10)
+            contentValues.put(KEY_INGREDIENT11, sharedBookingObject?.strIngredient11)
+            contentValues.put(KEY_INGREDIENT12, sharedBookingObject?.strIngredient12)
+            contentValues.put(KEY_INGREDIENT13, sharedBookingObject?.strIngredient13)
+            contentValues.put(KEY_INGREDIENT14, sharedBookingObject?.strIngredient14)
+            contentValues.put(KEY_INGREDIENT15, sharedBookingObject?.strIngredient15)
+            contentValues.put(KEY_MEASURE1, sharedBookingObject?.strMeasure1)
+            contentValues.put(KEY_MEASURE2, sharedBookingObject?.strMeasure2)
+            contentValues.put(KEY_MEASURE3, sharedBookingObject?.strMeasure3)
+            contentValues.put(KEY_MEASURE4, sharedBookingObject?.strMeasure4)
+            contentValues.put(KEY_MEASURE5, sharedBookingObject?.strMeasure5)
+            contentValues.put(KEY_MEASURE6, sharedBookingObject?.strMeasure6)
+            contentValues.put(KEY_MEASURE7, sharedBookingObject?.strMeasure7)
+            contentValues.put(KEY_MEASURE8, sharedBookingObject?.strMeasure8)
+            contentValues.put(KEY_MEASURE9, sharedBookingObject?.strMeasure9)
+            contentValues.put(KEY_MEASURE10, sharedBookingObject?.strMeasure10)
+            contentValues.put(KEY_MEASURE11, sharedBookingObject?.strMeasure11)
+            contentValues.put(KEY_MEASURE12, sharedBookingObject?.strMeasure12)
+            contentValues.put(KEY_MEASURE13, sharedBookingObject?.strMeasure13)
+            contentValues.put(KEY_MEASURE14, sharedBookingObject?.strMeasure14)
+            contentValues.put(KEY_MEASURE15, sharedBookingObject?.strMeasure15)
 
 
+            writableDatabase.insert(TABLE_DRINKS, null, contentValues)
+
+        }
 
 
     }
+
     fun displayIngredient(ingredient: List<String>?) {
-       val adapter = IngredientsRecyclerViewAdapter(this, ingredient!! )
+        val adapter = IngredientsRecyclerViewAdapter(this, ingredient!!)
         recycler_ingredients.adapter = adapter
 
     }
+
     fun displayMeasure(measure: List<String>?) {
-        val adapter = MeasureRecyclerViewAdapter( measure!! )
+        val adapter = MeasureRecyclerViewAdapter(measure!!)
         recycler_measure.adapter = adapter
 
     }
